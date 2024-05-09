859d4a7f275b9bc241d5e4dca69a9fe64593ba5a
jaderson.brandao <jaderson.brandao@gmail.com>
2024-05-08 22:15:45 -0300

    Mapeamento das entidades

---
+++ course/src/main/java/com/ead/course/models/LessonModel.java
@@ -0,0 +1,34 @@
+package com.ead.course.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_LESSONS")
+public class LessonModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID lessonId;
+    @Column(nullable = false, length = 150)
+    private String title;
+    @Column(nullable = false, length = 250)
+    protected String description;
+    @Column(nullable = false)
+    private String videoUrl;
+    @JsonFormat(shape = JsonFormat.Shape.SCALAR, pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+
+}
