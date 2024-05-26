fad350b409d40c911286ceeb1ed899a5a2073154
jaderson.brandao <jaderson.brandao@gmail.com>
2024-05-26 17:54:11 -0300

    Criação SpecificationTemplate course, module e lesson

--- course/src/main/java/com/ead/course/controllers/ModuleController.java
+++ course/src/main/java/com/ead/course/controllers/ModuleController.java
@@ -5,15 +5,19 @@ import com.ead.course.models.CourseModel;
 import com.ead.course.models.ModuleModel;
 import com.ead.course.services.CourseService;
 import com.ead.course.services.ModuleService;
+import com.ead.course.specifications.SpecificationTemplate;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -69,8 +73,10 @@ public class ModuleController {
     }
 
     @GetMapping("/courses/{courseId}/modules")
-    ResponseEntity<List<ModuleModel>> getAllModules(@PathVariable("courseId") UUID courseId) {
-        return ResponseEntity.ok(this.moduleService.findAllByCourse(courseId));
+    ResponseEntity<Page<ModuleModel>> getAllModules(@PathVariable("courseId") UUID courseId,
+                                                    SpecificationTemplate.ModuleSpec spec,
+                                                    @PageableDefault(page = 0, size = 10, sort = "moduleId", direction = Sort.Direction.ASC) Pageable pageable) {
+        return ResponseEntity.ok(this.moduleService.findAllByCourse(SpecificationTemplate.moduleCourseId(courseId).and(spec), pageable));
     }
 
     @GetMapping("/courses/{courseId}/modules/{moduleId}")
--- course/src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
@@ -3,6 +3,9 @@ package com.ead.course.services.impl;
 import com.ead.course.models.LessonModel;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.services.LessonService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -36,6 +39,11 @@ public class LessonServiceImpl implements LessonService {
     @Override
     public List<LessonModel> findAllLessonsIntoModule(UUID moduleId) {
         return this.lessonRepository.findAllLessonsIntoModule(moduleId);
+    }
+
+    @Override
+    public Page<LessonModel> findAllByModule(Specification<LessonModel> spec, Pageable pageable) {
+        return this.lessonRepository.findAll(spec, pageable);
     }
 
 }
