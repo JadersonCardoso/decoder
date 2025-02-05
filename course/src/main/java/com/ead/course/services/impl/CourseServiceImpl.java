a04b2ecb69b5636985c74c9fbbc4483b63531bd9
jaderson.brandao <jaderson.brandao@gmail.com>
2025-02-05 17:02:38 -0300

    refact<course>: refatoração no recursos, services e repositories para retirada das depências do couse service.

--- course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,13 +1,12 @@
 package com.ead.course.services.impl;
 
 import com.ead.course.models.CourseModel;
-import com.ead.course.models.CourseUserModel;
 import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
-import com.ead.course.repositories.CourseUserRepository;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
+import com.ead.course.repositories.UserRepository;
 import com.ead.course.services.CourseService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,14 +25,15 @@ public class CourseServiceImpl implements CourseService {
     private final ModuleRepository moduleRepository;
     private final LessonRepository lessonRepository;
 
-    private final CourseUserRepository courseUserRepository;
+    private final UserRepository courseUserRepository;
 
-    public CourseServiceImpl(CourseRepository courseRepository, ModuleRepository moduleRepository, LessonRepository lessonRepository, CourseUserRepository courseUserRepository) {
+    public CourseServiceImpl(CourseRepository courseRepository, ModuleRepository moduleRepository, LessonRepository lessonRepository, UserRepository courseUserRepository) {
         this.courseRepository = courseRepository;
         this.moduleRepository = moduleRepository;
         this.lessonRepository = lessonRepository;
         this.courseUserRepository = courseUserRepository;
     }
+
     @Transactional
     @Override
     public void delete(CourseModel courseModel) {
@@ -46,10 +46,6 @@ public class CourseServiceImpl implements CourseService {
                 }
             });
             this.moduleRepository.deleteAll(moduleModelList);
-        }
-        List<CourseUserModel> courseUserModelList = this.courseUserRepository.findAllCourseUserIntCourse(courseModel.getCourseId());
-        if (!courseUserModelList.isEmpty()) {
-            this.courseUserRepository.deleteAll(courseUserModelList);
         }
         this.courseRepository.delete(courseModel);
     }
--- course/src/main/java/com/ead/course/specifications/SpecificationTemplate.java
+++ course/src/main/java/com/ead/course/specifications/SpecificationTemplate.java
@@ -1,7 +1,7 @@
 package com.ead.course.specifications;
 
 import com.ead.course.models.CourseModel;
-import com.ead.course.models.CourseUserModel;
+import com.ead.course.models.UserModel;
 import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
 import jakarta.persistence.criteria.Expression;
@@ -48,11 +48,4 @@ public class SpecificationTemplate {
         });
     }
 
-    public static Specification<CourseModel> courseUserId(final UUID userId) {
-        return (root, query, cb) -> {
-            query.distinct(true);
-            Join<CourseModel, CourseUserModel> courseprod = root.join("coursesUsers");
-            return cb.equal(courseprod.get("userId"), userId);
-        };
-    }
 }
