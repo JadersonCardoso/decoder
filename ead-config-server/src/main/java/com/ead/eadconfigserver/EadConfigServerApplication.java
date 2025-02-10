59d3e2c46afc6e82ad3dbab34735bd18c2577872
jaderson.brandao <jaderson.brandao@gmail.com>
2025-02-10 15:14:45 -0300

    feat<config-sever>: criação do projeto ead-config-server para aplicação do global config management

---
+++ ead-config-server/src/main/java/com/ead/eadconfigserver/EadConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.ead.eadconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadConfigServerApplication.class, args);
+	}
+
+}
